7230ddcba50916397c700969faf9129a366e0207
Hans H. Sievertsen <hhsievertsen@users.noreply.github.com>
2025-01-27 22:24:19 +0100

    Create exploredata.R

---
+++ exploredata.R
@@ -0,0 +1,14 @@
+options("width"=150)
+# read trees
+
+df<-arrow::read_parquet("https://github.com/hhsievertsen/kbhtrae/raw/refs/heads/main/data/trae_basis.parquet")
+
+
+df_Strødamvej<-df[df$vejnavn=="Lundehusvej",]
+df_EmdrupSoe<-df[df$stednavn%in%c("Emdrup Sø","Emdrup Søpark"),]
+
+table(df_EmdrupSoe$traeart)
+table(df_EmdrupSoe$dansk_navn)
+table(df_EmdrupSoe$slaegtsnavn)
+table(df_EmdrupSoe$slaegt)
+View(df_EmdrupSoe)
